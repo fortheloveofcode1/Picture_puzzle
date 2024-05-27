4f6e797626df95d1606c96697d337b396702918e
priyanka Mukherjee <priyankamukherjee@priyankas-MacBook-Air.local>
2024-05-27 16:02:27 +0530

    first version

---
+++ script.js
@@ -0,0 +1,70 @@
+var rows = 3;
+var columns = 3;
+
+var curTile;
+var otherTile;
+
+var turns = 0;
+
+var correctOrder = ["9", "8", "7", "6", "5", "4", "3", "2", "1"];
+var imgOrder = ["8", "5", "1", "4", "3", "7", "2", "6", "9"];
+window.onload = function () {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let tile = document.createElement("img");
+            tile.id = i.toString() + "-" + j.toString();
+            tile.src = imgOrder.shift() + ".jpg";
+            tile.addEventListener("dragstart", dragStart);
+            tile.addEventListener("dragover", dragOver);
+            tile.addEventListener("dragenter", dragEnter);
+            tile.addEventListener("dragleave", dragleave);
+            tile.addEventListener("drop", dragDrop);
+            tile.addEventListener("dragend", dragEnd);
+
+            document.getElementById("board").appendChild(tile);
+        }
+    }
+}
+function dragStart() {
+    curTile = this;
+}
+function dragOver(e) {
+    e.preventDefault();
+}
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragleave(e) {
+    e.preventDefault();
+}
+function dragDrop() {
+    otherTile = this;
+}
+function dragEnd() {
+    let currImg = curTile.src;
+    let otherImg = otherTile.src;
+
+    curTile.src = otherImg;
+    otherTile.src = currImg;
+
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
+    if (checkOrder()) {
+        setTimeout(() => {
+            alert("Congratulations! You solved the puzzle.");
+        }, 100);
+    }
+
+}
+function checkOrder() {
+    let currentOrder = [];
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let tile = document.getElementById(i.toString() + "-" + j.toString());
+            let src = tile.src.split("/").pop().split(".")[0]; // Get the image number
+            currentOrder.push(src);
+        }
+    }
+    return JSON.stringify(currentOrder) === JSON.stringify(correctOrder);
+}
